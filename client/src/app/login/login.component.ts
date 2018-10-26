da978d6905d2ef0f65cdb395bfcc4fb01fb5aa15
Rohit Prasad <rohitrp3@gmail.com>
2018-10-26 23:31:15 +0530

    Fix bug

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -9,8 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email: string = 'rp@rp.com';
-  password: string = 'rp';
+  email: string = '';
+  password: string = '';
 
   public login() {
     this.authService.login(this.email, this.password)
